95124cad69f6019c4d27d1ebb6a4a53bc99bf93e
Farooquekk <22sw040@students.muet.edu.pk>
2024-08-12 22:04:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+gsap.from(".container h2", {
+    x: 300,
+    y: 200,
+    duration: 2,
+    delay:0.5,
+    ease:"bounce"
+})
+
+gsap.from(".container p", {
+    x: -350,
+    y: -200,
+    duration: 2,
+    delay:3,
+    ease:"back"
+})
+
